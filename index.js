9fbd4eecfefe330fec58f9283d4e3ca90dd61b5c
naimulabeer <naimul.h.abeer@gmail.com>
2023-11-05 14:32:35 +0600

    mongodb connected in my index file

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygqixms.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Listening from port", port);
