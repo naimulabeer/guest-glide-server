1e690d17143d23495d9a95a6eed5f247913d63c0
naimulabeer <naimul.h.abeer@gmail.com>
2023-11-06 02:09:22 +0600

    get route for room ids

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,13 @@ async function run() {
     app.get("/rooms", async (req, res) => {
       const rooms = roomsCollection.find();
       const result = await rooms.toArray();
+      res.send(result);
+    });
+
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.findOne(query);
       res.send(result);
     });
 
